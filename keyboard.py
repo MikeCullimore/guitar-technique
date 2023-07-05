5c05100bc68c1d65f9ce2ab5b2f2049f9b7d2d06
Mike Cullimore <michael.cullimore11@gmail.com>
2023-07-05 23:23:36 +0100

    Make keyboard span multiple octaves (hacky!).

--- keyboard.py
+++ keyboard.py
@@ -2,11 +2,22 @@
 Draw images of a piano keyboard with given notes marked.
 
 TODO:
+Animate. GIF? Video? (webm?)
+Combine with fretboard in single image.
 Allow arbitrary first and last key.
+    Work out range of guitar in standard tuning with same # frets as own (22?) and match it. C2 to C6?
+    Default to range of full 88 key piano (because both ends are odd!). Simpler to just do whole octaves?
+    Do same for bass.
 Define vertices for each note then repeat for octaves.
     White notes should include shape of black not just rectangle drawn over.
+    Check by colouring each key blue separately.
+    Function to draw note: input note and colour (including transparency).
+        Separate border and fill?
 Operator overloads for point (add, subtract).
 Option to print note names on keys.
+    Or scale degrees.
+Encapsulate coordinate transformations in drawing functions.
+SVG equivalent (for React).
 """
 
 from dataclasses import dataclass
@@ -15,10 +26,12 @@ from typing import List
 
 import cairo
 
+
+# TODO: use this to define range and address individual notes (e.g. to colour).
 @dataclass
-class NoteMarker:
-    string: int
-    fret: int
+class Note:
+    noteName: str
+    octave: int
 
 @dataclass
 class Point:
@@ -30,10 +43,18 @@ class Point:
 
 
 # TODO: no global state!
+
+# Phone screen dimensions.
 image_width = 2220
 image_height = 1080
-# TODO: optimise for tablet and laptop dimensions also.
 
+# Tablet screen dimensions.
+# image_width = 2160
+# image_height = 1620
+
+# Tablet screen dimensions.
+# image_width = 2560
+# image_height = 1600
 
 def get_transforms(top_left: Point, bottom_right: Point):
     # TODO: do we need global_to_normed?
@@ -80,22 +101,26 @@ def main():
     context.set_antialias(cairo.ANTIALIAS_BEST)
 
     # Set line width and color.
-    black = (0, 0, 0)
+    black_key_colour = (0, 0, 0)
+    # t = 0.7
+    # black_key_colour = (t, t, t)
     line_width = 5  # TODO: scale with image.
     context.set_line_width(line_width)
-    context.set_source_rgb(*black)
+    context.set_source_rgb(*black_key_colour)
 
     # Establish coordinate system transforms.
     # TODO: make padding a ratio of image size not absolute pixels.
+    # TODO: enforce aspect ratio limits for keyboard so never squashed.
     padding_horizontal = 200
     padding_vertical = 100
     top_left = Point(padding_horizontal, padding_vertical)
-    bottom_right = Point(image_width - padding_horizontal, image_height - padding_vertical)
+    bottom_right = Point(image_width - padding_horizontal, image_height/2)  #  - padding_vertical)
     _, normed_to_global = get_transforms(top_left, bottom_right)
 
     # Define keyboard dimensions.
     golden_ratio = 2/(1 + math.sqrt(5))
-    num_white_keys = 7
+    num_octaves = 4
+    num_white_keys = 7*num_octaves
     white_key_width = 1/num_white_keys
     black_key_width = golden_ratio*white_key_width
     
@@ -113,11 +138,15 @@ def main():
         draw_polygon(context, vertices, fill=False)
     
     # Draw black keys.
-    c = 0.01
-    d = 0.02
-    offsets = [-c, c, -d, 0, d]
-    for k, offset in zip([1, 2, 4, 5, 6], offsets):
-        left = k*white_key_width - black_key_width/2 + offset
+    c = black_key_width/4
+    d = c
+    offsets = num_octaves*[-c, c, -d, 0, d]
+    multiples = []
+    for octave in range(num_octaves):
+        for black_key_position in [1, 2, 4, 5, 6]:
+            multiples.append(7*octave + black_key_position)
+    for multiple, offset in zip(multiples, offsets):
+        left = multiple*white_key_width - black_key_width/2 + offset
         right = left + black_key_width
         vertices = normed_to_global_list([
                 Point(left, 0),
